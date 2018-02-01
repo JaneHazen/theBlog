e0193d5e864c78cf57981d179711abcdb562ec1b
Jane <iamjane.gmail.com>
2018-02-01 12:58:04 -0800

    set up redux form and field inputs

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+
+  renderField(field){
+    return(
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input
+          className="form-control"
+          type="text"
+          {...field.input}
+        />
+      </div>
+    )
+  }
+
   render(){
     return(
-      <div>
-        NewPOsts
-      </div>
+      <form>
+        <Field
+          name="title"
+          label="Title"
+          component={this.renderField}
+        />
+        <Field
+          name="tags"
+          label="Tags"
+          component={this.renderField}
+        />
+        <Field
+          name="content"
+          label="Post Content"
+          component={this.renderField}
+        />
+      </form>
     )
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew);
