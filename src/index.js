5965ccecffc1da1b9450e7408705d94c8041194c
Jane <iamjane.gmail.com>
2018-02-01 11:27:00 -0800

    initial commit, create-react-app set up

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import App from './components/App';
+import reducers from './reducers';
+
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+class Hello extends React.Component {
+  render() { return <div> Hello! </div>}
+}
+
+class Goodbye extends React.Component {
+  render() { return <div> Goodbye! </div>}
+}
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('#root'));
