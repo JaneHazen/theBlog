c32f68872ca2f7674613ac83236a43a2696a4fa1
janehazen <iamjane@gmail.com>
2018-02-01 12:34:20 -0800

    Revert "add new post link and button to navigate"

--- src/index.js
+++ src/index.js
@@ -2,25 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import promise from 'redux-promise';
-import reducers from './reducers';
-// components
 import PostsIndex from './components/posts_index';
-import PostsNew from './components/posts_new';
+import reducers from './reducers';
 
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+// components
 
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Switch>
-          <Route path="/posts/new" component={ PostsNew } />
-          <Route path="/" component={ PostsIndex } />
-        </Switch>
+        <Route exact path="/" component={ PostsIndex } />
       </div>
     </BrowserRouter>
   </Provider>
