30eabbdd142476ae7bebd9e142acac0db4dfdc6d
Jane <iamjane.gmail.com>
2018-02-01 16:17:15 -0800

    add CSS to blog

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+class Header extends Component {
+  render(){
+    return(
+      <div className="container">
+        <div className="jumbotron">
+          <h1 className="headerstyle">Blogs4Days: where you blog to blog</h1>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Header;
